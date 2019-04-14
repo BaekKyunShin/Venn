e1314312f7b8ec5c297590ef94b9afac3ab2e00c
bkshin <werooring@gmail.com>
2019-04-14 12:38:02 +0900

    Add the infinite scrolling

--- app/src/main/java/com/example/weroo/becksco/home/HomeActivity.kt
+++ app/src/main/java/com/example/weroo/becksco/home/HomeActivity.kt
@@ -2,10 +2,10 @@ package com.example.weroo.becksco.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.widget.AbsListView
 import android.widget.Toast
 import com.example.weroo.becksco.R
 import com.example.weroo.becksco.api.DefaultPrefHelper
@@ -19,24 +19,16 @@ import kotlinx.android.synthetic.main.activity_home.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import android.util.Log
-import android.view.View
-import android.widget.ProgressBar
-import com.github.ybq.android.spinkit.style.DoubleBounce
-import com.github.ybq.android.spinkit.sprite.Sprite
-
-
-
 
 
 class HomeActivity : AppCompatActivity() {
 
     private val postSummaryAdapter = PostSummaryAdapter()
-    private var scrollState: Int = 0 // 0 = idle state, 1 = End of List, -1 = Top of List
     private var currentItems: Int = 0
-    private var totalItems: Int = 0
     private var scrollOutItems: Int = 0
     private var pageNum: Int = 0
+
+    private val SCROLL_DIRECTION_BOTTOM: Int = 1
 
 
     // AppCompatActivity()의 onCreate를 overrding, Super은 상위 class를 말함, 즉 AppCompatActivity()
@@ -44,22 +36,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // apply a spin kit
-        val doubleBounce = DoubleBounce()
-        spin_kit.setIndeterminateDrawable(doubleBounce)
-
         // Data Binding
         bindData()
     }
 
-    // 화면이 가려졌다가 다시 불리면 onResume이 불려 서버에 저장된 데이터를 다시 가져옴
-    override fun onResume() {
-        super.onResume()
-        bindData()
-    }
-
     private fun bindData() {
-        floatingActionButton.setOnClickListener{
+        floatingActionButton.setOnClickListener {
             startActivity(Intent(this, PostCreateActivity::class.java))
         }
         // activity_home의 postSummaryList id 값
@@ -69,104 +51,56 @@ class HomeActivity : AppCompatActivity() {
 
         // adapter로는 postSummaryAdapter 설정
         postSummaryList.adapter = postSummaryAdapter
-        requestHome(0)
+        requestHome()
 
         postSummaryList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-           override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-               super.onScrollStateChanged(recyclerView, newState)
-               if (!postSummaryList.canScrollVertically(-1)) {
-                   Log.i("TopOfList", "Top of list")
-                   spin_kit.visibility = View.VISIBLE
-                   goToPage(-1)
-               } else if (!postSummaryList.canScrollVertically(1)) {
-                   Log.i("EndOfList", "End of list")
-                   spin_kit.visibility = View.VISIBLE
-                   goToPage(1)
-               } else {
-                   Log.i("idleState", "idle")
-               }
-
-               //if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
-               //    isScrolling = true
-               //}
-           }
-            /**
-           override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-               super.onScrolled(recyclerView, dx, dy)
-               // currentItems = LinearLayoutManager(this@HomeActivity)!!.childCount
-
-               if (scrollState == -1) {
-                   scrollState = 0
-                   goToPage(-1)
-               } else if (scrollState == 1) {
-                   scrollState = 0
-                   goToPage(1)
-               }
-
-                totalItems = LinearLayoutManager(this@HomeActivity)!!.itemCount
-                scrollOutItems = LinearLayoutManager(this@HomeActivity)!!.findFirstVisibleItemPosition()
-
-                if (isScrolling && (currentItems + scrollOutItems == totalItems)) {
-                    isScrolling = false
-
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (!postSummaryList.canScrollVertically(SCROLL_DIRECTION_BOTTOM)) {
+                    requestHome()
                 }
-
-           }
-             */
-       })
-    }
-
-    private fun goToPage(scrollState: Int) {
-        if (pageNum < 0) {
-            requestHome(0)
-            pageNum = 0
-        } else if (pageNum > 3) {
-            requestHome(2)
-            pageNum = 2
-        } else {
-            pageNum += scrollState
-            requestHome(pageNum)
-        }
-    }
-
-
-    private fun requestHome(page: Int) {
-        postApi.getPosts(page).enqueue(object : Callback<Page<PostSummaryDTO>> {
-            override fun onResponse(call: Call<Page<PostSummaryDTO>>, response: Response<Page<PostSummaryDTO>>) {
-                if (response.code() != 200) {
-                    Toast.makeText(this@HomeActivity, "What the .. 201", Toast.LENGTH_SHORT).show()
-                    return
-                }
-                val content = response.body()?.content
-                if (content == null) {
-                    signOut()
-                    return
-                }
-
-                postSummaryAdapter.resetPosts(response.body()?.content ?: listOf())
-
-                // data가 바뀌었다고 view에 알려주는 역할
-                postSummaryAdapter.notifyDataSetChanged()
-
-                //totalItems = postSummaryList.layoutManager!!.itemCount
-                currentItems = LinearLayoutManager(this@HomeActivity).childCount
-                scrollOutItems = LinearLayoutManager(this@HomeActivity).findFirstVisibleItemPosition()
-                //Log.d("total", totalItems.toString())
-                Log.d("scrolloutItems", scrollOutItems.toString())
-
-                Toast.makeText(this@HomeActivity, "currentItems", Toast.LENGTH_SHORT).show()
-                spin_kit.visibility = View.GONE
             }
-
-            override fun onFailure(call: Call<Page<PostSummaryDTO>>, t: Throwable) {
-                Toast.makeText(this@HomeActivity, "What the ..", Toast.LENGTH_SHORT).show()
-            }
-
         })
     }
+
+    private fun requestHome() {
+
+        postSummaryAdapter.setLoading(true)
+        postSummaryAdapter.notifyDataSetChanged()
+        Handler().postDelayed({
+            postApi.getPosts(pageNum).enqueue(object : Callback<Page<PostSummaryDTO>> {
+                override fun onResponse(call: Call<Page<PostSummaryDTO>>, response: Response<Page<PostSummaryDTO>>) {
+                    if (response.code() != 200) {
+                        Toast.makeText(this@HomeActivity, "What the .. 201", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+                    val content = response.body()?.content
+                    if (content == null) {
+                        signOut()
+                        return
+                    }
+
+                    ////////////
+                    pageNum++;
+
+                    postSummaryAdapter.appendPosts(response.body()?.content ?: listOf())
+                    postSummaryAdapter.setLoading(false)
+
+                    // data가 바뀌었다고 view에 알려주는 역할
+                    postSummaryAdapter.notifyDataSetChanged()
+                }
+
+                override fun onFailure(call: Call<Page<PostSummaryDTO>>, t: Throwable) {
+                    postSummaryAdapter.setLoading(false)
+                    Toast.makeText(this@HomeActivity, "What the ..", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }, 3000)
+    }
+
     private fun signOut() {
         DefaultPrefHelper.instance().setBoolean(SIGN_PREFERENCE, false)
         startActivity(Intent(this, SignActivity::class.java))
         finish()
     }
-    }
+}
--- app/src/main/java/com/example/weroo/becksco/home/PostSummaryAdapter.kt
+++ app/src/main/java/com/example/weroo/becksco/home/PostSummaryAdapter.kt
@@ -8,33 +8,69 @@ import android.widget.TextView
 import com.example.weroo.becksco.R
 import com.example.weroo.becksco.model.PostSummaryDTO
 import kotlinx.android.synthetic.main.item_post_summary.view.*
+import retrofit2.http.POST
+import java.lang.IllegalArgumentException
 
 // recycler view :
 
-class PostSummaryAdapter: RecyclerView.Adapter<PostSummaryAdapter.PostSummaryHolder>() {
+class PostSummaryAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val posts: MutableList<PostSummaryDTO> by lazy { mutableListOf<PostSummaryDTO>() }
 
+    private var loading = false
+
+    private val POST_VIEW = 0
+    private val LOADING_VIEW = 1
+
+    fun setLoading(loading: Boolean) {
+        this.loading = loading;
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(!loading) {
+            return POST_VIEW
+        }
+        if (position < posts.size) {
+            return POST_VIEW;
+        }
+        return LOADING_VIEW
+    }
+
     // RecyclerView의 행을 표시하는데 사용되는 layout xml을 가져오는 역할
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): PostSummaryHolder {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         // viewGroup: view를 자식으로 가질 수 있는 Group
-        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_post_summary, viewGroup, false)
-        return PostSummaryHolder(v)
+        val inflater: LayoutInflater = LayoutInflater.from(viewGroup.context);
+        if (viewType == POST_VIEW) {
+            return PostSummaryHolder(inflater.inflate(R.layout.item_post_summary, viewGroup, false));
+        }
+        if (viewType == LOADING_VIEW) {
+            return LoadingHolder(inflater.inflate(R.layout.layout_toolbar, viewGroup, false));
+        }
+        throw IllegalArgumentException()
     }
 
     // RecyclerView의 행 갯수 리턴
     override fun getItemCount(): Int {
+        if (loading) {
+            return posts.size + 1
+        }
         return posts.size
     }
 
     // 마침내 RecyclerView의 행에 보여질 TextView를 binding
-    override fun onBindViewHolder(holder: PostSummaryHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         // posts MutableList에서 position index번째의 PostSummaryDTO를 바인딩
-        holder.bind(posts[position])
+        if (holder is PostSummaryHolder) {
+            holder.bind(posts.get(position))
+            return
+        }
+        if (holder is LoadingHolder) {
+            return
+        }
+        throw IllegalArgumentException()
     }
 
-    fun resetPosts(posts: List<PostSummaryDTO>) {
-        this.posts.clear()
+    fun appendPosts(posts: List<PostSummaryDTO>) {
         this.posts.addAll(posts)
     }
 
@@ -47,5 +83,9 @@ class PostSummaryAdapter: RecyclerView.Adapter<PostSummaryAdapter.PostSummaryHol
             postSummaryTitle.text = postSummary.title
             postSummaryWriter.text = postSummary.user.name
         }
+    }
+
+    class LoadingHolder(view: View): RecyclerView.ViewHolder(view) {
+
     }
 }
